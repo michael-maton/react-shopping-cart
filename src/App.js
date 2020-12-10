312c14c6df7c535eab30df678c93b2b73399a7b4
Michael Maton <mjmaton6@gmail.com>
2020-12-10 14:07:29 -0600

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+
+// Contexts
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = (item) => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={cart}>
+          <Navigation />
+		</CartContext.Provider>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+          <Route path="/cart">
+		  	<CartContext.Provider value={cart}>
+            	<ShoppingCart />
+			</CartContext.Provider>
+          </Route>
+        
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
